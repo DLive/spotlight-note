81a92b62161e8c4dde0cf381b6912bef4a72dead
dlive <xsxgmail@163.com>
2013-03-31 22:53:16 +0800

    Initial Commit

---
+++ noteBook/MySpotlightImporter.h
@@ -0,0 +1,19 @@
+//
+//  MySpotlightImporter.h
+//  noteBook
+//
+//  Created by DLive on 13-3-31.
+//  Copyright (c) 2013年 DLive. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MySpotlightImporter : NSObject
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)attributes error:(NSError **)error;
+
+@end
